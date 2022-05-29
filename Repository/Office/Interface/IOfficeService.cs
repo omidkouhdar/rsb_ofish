777042182927f9d353af71b772620a95a3df2859
omid kouhdar <kouhdar@rsb.ir>
2022-05-29 16:01:27 +0430

    Add Search Section

---
+++ Repository/Office/Interface/IOfficeService.cs
@@ -0,0 +1,14 @@
+﻿using RSB_Ofish_System.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RSB_Ofish_System.Repository.Office.Interface
+{
+    public interface IOfficeService : IDisposable
+    {
+        Task<List<SelectListItemVM>> GetList();
+        
+    }
+}
---
+++ Repository/Office/Service/OfficeService.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RSB_Ofish_System.Data;
+using RSB_Ofish_System.Models.ViewModels;
+using RSB_Ofish_System.Repository.Office.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RSB_Ofish_System.Repository.Office.Service
+{
+    public class OfficeService : IOfficeService
+    {
+        private readonly RSB_Ofish_SystemContext _context;
+        public OfficeService(RSB_Ofish_SystemContext context)
+        {
+            _context = context;
+        }
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+        public async Task<List<SelectListItemVM>> GetList()
+        {
+            var list = new List<SelectListItemVM> { new SelectListItemVM
+            {
+                Id = "0" , Name = "انتخاب کنید"
+            }
+            };
+            var model = await _context.Office.Select(s => new SelectListItemVM { Id = s.Id.ToString(), Name = s.Name }).ToListAsync();
+            foreach(var item in model)
+            {
+                list.Add(new SelectListItemVM
+                {
+                    Id = item.Id,
+                    Name = item.Name
+                });
+            }
+            return list;
+        }
+    }
+}
